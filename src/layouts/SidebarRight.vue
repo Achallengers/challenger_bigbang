4e094ef3c14e6f5069f9b489d4877351c2dafc23
samuel-jesus <samuel.jesan2018@gmail.com>
2023-08-02 13:38:30 -0300

    fix: import component card for sidebarrigth

--- src/layouts/SidebarRight.vue
+++ src/layouts/SidebarRight.vue
@@ -3,12 +3,22 @@
     <h6>Tarefas de Hoje</h6>
     <div class="row">
       <div class="col-12">
-    
+        <Card></Card>
       </div>
     </div>
     <h6>Projeto Recente</h6>
   </div>
 </template>
+
+<script scoped>
+import Card from "../components/card.vue";
+
+export default {
+  components: {
+    Card,
+  },
+};
+</script>
 
 <style>
 .sidebar-right {
