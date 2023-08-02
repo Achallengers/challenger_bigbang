ee702fbd40d8453f7e255014a6fcc62da13893ca
samuel-jesus <samuel.jesan2018@gmail.com>
2023-08-02 13:42:15 -0300

    feat: Created app layout and fix problems

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,39 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import "./assets/grid.css";
+import "./assets/flex.css";
+import "./assets/responsive_text.css";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+import SidebarRight from "./layout/SidebarRight.vue";
+import Sidebar from "./layout/Sidebar.vue";
+import MainView from "./layout/MainView.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="dashboard">
+    <Sidebar />
+    <MainView />
+    <SidebarRight />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.dashboard {
+  display: grid;
+  grid-template-columns: 16% 60% 24%;
+  height: 100%;
+  grid-auto-rows: minmax(100%, 100%);
+  min-height: 100vh;
   max-height: 100vh;
+  font-family: Nunito, sans-serif;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.sidebar,
+.main-content {
+  padding: 10px;
+  border: 1px solid #ccc;
 }
 </style>
