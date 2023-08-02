15fcc8d38c7116852cae64d9b8a2ca047f3a563c
samuel-jesus <samuel.jesan2018@gmail.com>
2023-08-02 13:37:57 -0300

    fix: corrected routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {
+  createRouter,
+  createWebHistory
+} from 'vue-router'
+import PanelView from '../views/PanelView.vue'
+import ProjectsView from '../views/ProjectsView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
+  routes: [{
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'panel',
+      component: PanelView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/projects',
+      name: 'projects',
+      component: ProjectsView
     }
   ]
 })
